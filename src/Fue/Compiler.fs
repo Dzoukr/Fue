584682ce0aefe21100e722f4732dca884577f49c
Roman Provazník <dzoukr@dzoukr.cz>
2016-12-06 16:00:57 +0100

    Still with failing tuple funcs

--- src/Fue/Compiler.fs
+++ src/Fue/Compiler.fs
@@ -6,6 +6,7 @@ open StringUtils
 open Microsoft.FSharp.Reflection
 
 let private isRecord obj = FSharpType.IsRecord(obj.GetType())
+let private isFunction obj = FSharpType.IsFunction(obj.GetType())
 let private getRecordValue key obj =
     FSharpType.GetRecordFields(obj.GetType()) 
     |> Array.filter (fun x -> x.Name = key) 
@@ -15,25 +16,34 @@ let private getRecordValue key obj =
 
 let private compileSimpleValue data key =
     match data |> Data.tryGet key with
-    | Some(value) -> value |> string // direct match
+    | Some(value) -> value // direct match
     | None ->
         let recName,props = key |> splitToFirstAndList '.'
         match props |> List.length with
-        | 0 -> "NOT FOUND TODO"
+        | 0 -> "NOT FOUND TODO" |> box
         | _ -> 
-            let foldFn (acc:obj * string) (item:string) =
+            let foldFn (acc:obj * obj) (item:string) =
                 let v = getRecordValue item <| fst acc
                 match v |> isRecord with
                 | true -> v, (snd acc)
-                | false -> (fst acc), (v |> string)
-            
-            let record = data |> get recName // |> getRecordValue (props.Head) |> string
-            let res = props |> List.fold foldFn (record, "")
-            snd res
+                | false -> (fst acc), v
+            let record = data |> get recName
+            props |> List.fold foldFn (record, obj()) |> snd
 
 let compile data value =
-    match value with
-    | SimpleValue(valueName) -> valueName |> compileSimpleValue data
-    | Function(fnName, pars) -> 
-        let fn = data |> get fnName
-        fnName
+    let parsOrUnit arr = 
+        match arr |> Array.length with
+        | 0 -> [| () |> box |]
+        | _ -> arr
+    
+    let rec comp v =
+        match v with
+        | SimpleValue(valueName) -> valueName |> compileSimpleValue data
+        | Function(fnName, pars) -> 
+            let p = pars |> List.map comp |> List.toArray |> parsOrUnit
+            let fn = data |> get fnName
+            let invoke = fn.GetType().GetMethod("Invoke")
+            invoke.Invoke(fn, p)
+
+            
+    comp value
--- tests/Fue.Tests/Compiler.fs
+++ tests/Fue.Tests/Compiler.fs
@@ -37,12 +37,29 @@ let ``Compiles nested record value`` () =
     let data = init |> add "rec" record
     SimpleValue("rec.Nested.Value")
     |> compile data
-    |> should equal "123"
+    |> should equal 123
 
 [<Test>]
-let ``Compiles simple parameterless function`` () = 
+let ``Compiles tuple function`` () = 
+    let tuple = "Roman", 35
+    let data = init |> add "tuple" tuple |> add "fst" fst
+    Function("fst", [SimpleValue("tuple")])
+    |> compile data
+    |> should equal "Roman"
+
+[<Test>]
+let ``Compiles parameterless function`` () = 
     let fun1 = fun() -> "Roman"
     let data = init |> add "fun1" fun1
     Function("fun1", [])
     |> compile data
     |> should equal "Roman"
+
+
+[<Test>]
+let ``Compiles function with param`` () = 
+    let fun1 = fun x -> x + 10
+    let data = init |> add "fun1" fun1 |> add "x" 90
+    Function("fun1", [SimpleValue("x")])
+    |> compile data
+    |> should equal 100
